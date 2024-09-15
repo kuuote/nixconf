650270a3628e6e7a912792de329d84aeea85de4e
kuuote <znmxodq1@gmail.com>
2024-09-15 10:58:36 +0900

    テンプレ大事

---
+++ flake-template/develop.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      packages.x86_64-linux.default = pkgs.mkShell {
+        packages = with pkgs; [
+          fish
+          hello
+        ];
+        shellHook = ''
+          exec fish
+        '';
+      };
+    };
+}
