9368a13dcf0a68b9db415833db4598fc6ce9da29
kuuote <znmxodq1@gmail.com>
2024-09-15 23:57:53 +0900

    environmentFileが消えたので追従

--- nixos/networking/wireless.nix
+++ nixos/networking/wireless.nix
@@ -1,10 +1,10 @@
 {
   # Pick only one of the below networking options.
   networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
-  networking.wireless.environmentFile = "/data/nix-secrets/wireless.env";
+  networking.wireless.secretsFile = "/data/nix-secrets/wireless.conf";
   networking.wireless.networks = {
     "Buffalo-A-230E" = {
-      psk = "@PSK_BUFFALO@";
+      pskRaw = "ext:psk_buffalo";
     };
   };
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
