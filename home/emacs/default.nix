a64f84b19d1bf1d74b562af853aa5c4d1e43953c
kuuote <znmxodq1@gmail.com>
2024-10-20 17:08:04 +0900

    init.orgにNixのパッケージ宣言を書けるようにした

--- home/emacs/default.nix
+++ home/emacs/default.nix
@@ -6,9 +6,41 @@
 let
   tangle = inputs.org-babel.lib.tangleOrgBabel { languages = [ "emacs-lisp" ]; };
   init = builtins.readFile ./init.org;
+  # init.orgから行頭に書かれた `nix: epkgs.ddskk` のような定義を抽出する
+  decls =
+    let
+      a = builtins.split "\nnix: ([a-zA-Z.]+)" init;
+      b = builtins.filter builtins.isList a;
+      c = builtins.map (list: builtins.elemAt list 0) b;
+    in
+    c;
+  # AttrSetと `epkgs.ddskk` のような式を渡すと再帰的に参照する
+  ref =
+    set: expr:
+    let
+      a = builtins.split "\\." expr;
+      b = builtins.filter builtins.isString a;
+      c = builtins.foldl' (set: attr: set."${attr}") set b;
+    in
+    c;
 in
 {
-  programs.emacs.enable = true;
+  programs.emacs = {
+    enable = true;
+    package = pkgs.emacsWithPackages (
+      epkgs:
+      let
+        set = {
+          inherit
+            epkgs
+            pkgs
+            inputs
+            ;
+        };
+      in
+      builtins.map (ref set) decls
+    );
+  };
   home = {
     file = {
       ".emacs.d/init.el".text = tangle init;
