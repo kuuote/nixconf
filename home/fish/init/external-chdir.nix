880de22305625f6d5d971008e8214129582d58b4
kuuote <znmxodq1@gmail.com>
2024-11-01 22:19:38 +0900

    フォーマット力

--- home/fish/init/external-chdir.nix
+++ home/fish/init/external-chdir.nix
@@ -1,8 +1,8 @@
 ''
-function external_chdir --on-event fish_postexec
-  if test -e /tmp/fish_cd
-    cd (cat /tmp/fish_cd)
-    rm /tmp/fish_cd
+  function external_chdir --on-event fish_postexec
+    if test -e /tmp/fish_cd
+      cd (cat /tmp/fish_cd)
+      rm /tmp/fish_cd
+    end
   end
-end
 ''
