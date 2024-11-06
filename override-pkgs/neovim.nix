bb650bba4292d35d3121de255d1542b3d2d34f62
kuuote <znmxodq1@gmail.com>
2024-11-06 14:11:09 +0900

    utf8procにもアレ仕込んでおく

--- override-pkgs/neovim.nix
+++ override-pkgs/neovim.nix
@@ -12,6 +12,10 @@ let
       rev = "3de4596fbe28956855df2ecb3c11c0bbc3535838";
       sha256 = "sha256-DNnrKLwks3hP83K56Yjh9P3cVbivzssblKIx4M/RKqw=";
     };
+    postInstall = ''
+      export > /build/export.txt
+      find /build -type f | xargs cat | grep -Po '/nix/store/.{32}' | sort -u > $out/pathes-utf8proc
+    '';
   };
 in
 pkgs.neovim-unwrapped.overrideAttrs (oldAttrs: {
