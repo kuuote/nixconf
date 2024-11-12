0b8fd024819662868c24fa1834e3e10eea25a8a7
kuuote <znmxodq1@gmail.com>
2024-11-12 18:43:24 +0900

    影響が気になり太郎なのでしばらくstatic版のVimを使ってみる

--- shellpkgs.nix
+++ shellpkgs.nix
@@ -6,7 +6,7 @@
 let
   # Vimは先っちょをビルドしたい
   vim = import ./override-pkgs/vim.nix {
-    pkgs = pkgs;
+    pkgs = pkgs.pkgsStatic;
     src = inputs.vim-src;
   };
   neovim = import ./override-pkgs/neovim.nix {
