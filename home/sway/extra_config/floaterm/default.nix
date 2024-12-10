e0a94d0e190cba354a633afc0e313cf92e7d3a2b
kuuote <znmxodq1@gmail.com>
2024-12-10 13:00:00 +0900

    これでいいか

--- home/sway/extra_config/floaterm/default.nix
+++ home/sway/extra_config/floaterm/default.nix
@@ -3,4 +3,5 @@
   bindsym Mod4+i exec XMODIFIERS=@im= ${./.}/floaterm_ime t ime
   bindsym Mod4+p exec XMODIFIERS=@im= ${./.}/floaterm t
   bindsym Mod4+t exec XMODIFIERS=@im= ${./.}/floaterm t t
+  bindsym Mod4+m exec XMODIFIERS=@im= ${./.}/floaterm_ime t media
 ''
