0da54ed659b021c2dd29b61deed44f29149ea25f
kuuote <znmxodq1@gmail.com>
2024-12-28 14:40:54 +0900

    今回のnixpkgsの変更またでかいな

--- override-pkgs/neovim.nix
+++ override-pkgs/neovim.nix
@@ -27,11 +27,12 @@ pkgs.neovim-unwrapped.overrideAttrs (oldAttrs: {
   # installPhaseでもビルド走るのでbuildPhaseを潰す
   buildPhase = "true";
   postInstall = ''
-    ${oldAttrs.postInstall}
+    ${oldAttrs.postInstall or ""}
     export > /build/export.txt
     find /build -type f | xargs cat | grep -Po '/nix/store/.{32}' | sort -u > $out/pathes-neovim
   '';
+  nativeInstallCheckInputs = [ ];
   # 全依存をexportで吐き出してるから出力チェック潰さないと死ぬ
   # ref: https://discourse.nixos.org/t/getting-is-not-allowed-to-refer-to-the-following-paths-since-upgrading-to-unstable/21743
-  outputChecks = null;
+  disallowedRequisites = [ ];
 })
