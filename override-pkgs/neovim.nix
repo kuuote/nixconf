d5a2bf915c48e67179edbca9766e97595e564cb9
kuuote <znmxodq1@gmail.com>
2025-01-04 11:22:31 +0900

    nixpkgsのutf8procでNeovimビルドできるようになったわーい

--- override-pkgs/neovim.nix
+++ override-pkgs/neovim.nix
@@ -2,28 +2,11 @@
   pkgs,
   src,
 }:
-let
-  # Neovimの先っちょが要求するのでとりあえず
-  utf8proc-latest = pkgs.utf8proc.overrideAttrs {
-    allowSubstitutes = false;
-    version = "latest";
-    src = pkgs.fetchFromGitHub {
-      owner = "JuliaStrings";
-      repo = "utf8proc";
-      rev = "3de4596fbe28956855df2ecb3c11c0bbc3535838";
-      sha256 = "sha256-DNnrKLwks3hP83K56Yjh9P3cVbivzssblKIx4M/RKqw=";
-    };
-    postInstall = ''
-      export > /build/export.txt
-      find /build -type f | xargs cat | grep -Po '/nix/store/.{32}' | sort -u > $out/pathes-utf8proc
-    '';
-  };
-in
 pkgs.neovim-unwrapped.overrideAttrs (oldAttrs: {
   allowSubstitutes = false;
   version = "latest";
   src = src;
-  buildInputs = oldAttrs.buildInputs ++ [ utf8proc-latest ];
+  buildInputs = oldAttrs.buildInputs ++ [ pkgs.utf8proc ];
   # installPhaseでもビルド走るのでbuildPhaseを潰す
   buildPhase = "true";
   postInstall = ''
