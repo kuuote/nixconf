642b7568a567e6f40c010c6a984bba0d46278651
kuuote <znmxodq1@gmail.com>
2025-02-07 19:24:05 +0900

    フォーマッター後付けすると事故りがち

--- home/fish/init/mru.py
+++ home/fish/init/mru.py
@@ -6,8 +6,8 @@ try:
 except Exception:
     data = []
 
-data.insert(0, sys.argv[2] + '\n')
+data.insert(0, sys.argv[2] + "\n")
 data = list(dict.fromkeys(data))
 
-with open(sys.argv[1], 'w') as h:
+with open(sys.argv[1], "w") as h:
     h.writelines(list(dict.fromkeys(data)))
