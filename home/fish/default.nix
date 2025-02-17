6eb4c7a181d4173ab988f309d408668a7a21c60b
kuuote <znmxodq1@gmail.com>
2025-02-17 17:32:24 +0900

    リファク太郎

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -10,44 +10,44 @@ let
     ;
 in
 {
-  programs.fish.enable = true;
-  programs.fish.functions = mergeAttrs [
-    (import ./functions/chdir.nix)
-    # (import ./functions/mru.nix { inherit pkgs; })
-    {
-      # nix shell使うとネストするのでネスト数を出す
-      fish_right_prompt = ''
-        set -l fish_count (ps | grep fish | wc -l)
-        if test "$fish_count" -ne 1;
-          echo "[$fish_count]"
-        end
-      '';
-    }
-  ];
-  programs.fish.shellAbbrs = importAttrs [
-    # keep-sorted start
-    ./abbr/deno.nix
-    ./abbr/fish.nix
-    ./abbr/git.nix
-    ./abbr/misc.nix
-    ./abbr/nix.nix
-    ./abbr/tmux.nix
-    ./abbr/trash-cli.nix
-    ./abbr/vim.nix
-    # keep-sorted end
-  ] args;
-  programs.fish.shellInit = builtins.concatStringsSep "\n" ([
-    # 強制的にsessionVariablesをリロードする
-    ''
-      set -gu __HM_SESS_VARS_SOURCED
-      source (grep -o '/nix/store/.*hm-session-vars.fish$' (status current-filename))
-    ''
-    (import ./init/mru.nix { inherit pkgs; })
-    (import ./init/external-chdir.nix)
-    "fish_add_path ~/cmd"
-    "fish_add_path ~/.nix-profile/bin"
-  ]);
-  programs.fish.interactiveShellInit = "echo in ${
-    if args ? "nixosConfig" then "NixOS" else "Standalone"
-  }";
+  programs.fish = {
+    enable = true;
+    functions = mergeAttrs [
+      (import ./functions/chdir.nix)
+      # (import ./functions/mru.nix { inherit pkgs; })
+      {
+        # nix shell使うとネストするのでネスト数を出す
+        fish_right_prompt = ''
+          set -l fish_count (ps | grep fish | wc -l)
+          if test "$fish_count" -ne 1;
+            echo "[$fish_count]"
+          end
+        '';
+      }
+    ];
+    shellAbbrs = importAttrs [
+      # keep-sorted start
+      ./abbr/deno.nix
+      ./abbr/fish.nix
+      ./abbr/git.nix
+      ./abbr/misc.nix
+      ./abbr/nix.nix
+      ./abbr/tmux.nix
+      ./abbr/trash-cli.nix
+      ./abbr/vim.nix
+      # keep-sorted end
+    ] args;
+    shellInit = builtins.concatStringsSep "\n" ([
+      # 強制的にsessionVariablesをリロードする
+      ''
+        set -gu __HM_SESS_VARS_SOURCED
+        source (grep -o '/nix/store/.*hm-session-vars.fish$' (status current-filename))
+      ''
+      (import ./init/mru.nix { inherit pkgs; })
+      (import ./init/external-chdir.nix)
+      "fish_add_path ~/cmd"
+      "fish_add_path ~/.nix-profile/bin"
+    ]);
+    interactiveShellInit = "echo in ${if args ? "nixosConfig" then "NixOS" else "Standalone"}";
+  };
 }
