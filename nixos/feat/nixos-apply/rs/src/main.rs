1899d30aeeb7534bab492f75c2c1eca010003eb0
kuuote <znmxodq1@gmail.com>
2025-02-18 14:56:26 +0900

    actionの確認とnix-env叩きをするようにした

--- nixos/feat/nixos-apply/rs/src/main.rs
+++ nixos/feat/nixos-apply/rs/src/main.rs
@@ -1,7 +1,6 @@
-use std::env;
-use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
+use std::{env, fs};
 
 fn check_switcher(path: &PathBuf) -> Option<()> {
     let buf = fs::read_to_string(path).ok()?;
@@ -14,11 +13,23 @@ fn main() {
     let mut args = env::args();
     let _ = args.next();
     let path = fs::canonicalize(args.next().unwrap()).unwrap();
-    println!("{:?}", path);
     let switcher = path.join("bin/switch-to-configuration");
     let action = args.next().unwrap();
     check_switcher(&switcher).unwrap();
     // setuidビット設定しててもsetuid叩かないと伝播しないらしい
     unsafe { libc::setuid(0) };
+    let install_profile = match &*action {
+        "boot" => true,
+        "switch" => true,
+        "test" => false,
+        _ => panic!("unknown action: {}", action),
+    };
+    if install_profile {
+        Command::new("nix-env")
+            .args(["-p", "/nix/var/nix/profiles/system", "--set"])
+            .arg(path)
+            .status()
+            .unwrap();
+    }
     Command::new(switcher).arg(action).status().unwrap();
 }
