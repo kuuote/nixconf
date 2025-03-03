697361e7e1792eb08b92f45c7b1626ca366aedbe
kuuote <znmxodq1@gmail.com>
2025-03-04 02:32:19 +0900

    ブートローダのエントリ削る
    thanks @yasunori0418

--- nixos/systemd-boot.nix
+++ nixos/systemd-boot.nix
@@ -1,5 +1,9 @@
 {
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot = {
+    enable = true;
+    # /boot溢れを防ぐためにエントリの数を制限する
+    configurationLimit = 5;
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 }
