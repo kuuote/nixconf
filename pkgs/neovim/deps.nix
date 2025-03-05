b6ac1c8082c3effcdaa358e29a914b0f319a2b28
kuuote <znmxodq1@gmail.com>
2025-03-06 00:31:22 +0900

    pipe-operatorsはいいぞ

--- pkgs/neovim/deps.nix
+++ pkgs/neovim/deps.nix
@@ -12,26 +12,26 @@ let
       inherit name;
       derivationArgs = env;
     };
-  deps = lib.pipe "${neovim-src}/cmake.deps/deps.txt" [
-    builtins.readFile
-    (builtins.split "[\n]")
-    (builtins.filter builtins.isString)
-    (builtins.map (builtins.match "([0-9A-Z_]+)_(URL|SHA256) (.+)"))
-    (builtins.filter builtins.isList)
-    (builtins.map (m: {
+  deps =
+    "${neovim-src}/cmake.deps/deps.txt"
+    |> builtins.readFile
+    |> builtins.split "[\n]"
+    |> builtins.filter builtins.isString
+    |> builtins.map (builtins.match "([0-9A-Z_]+)_(URL|SHA256) (.+)")
+    |> builtins.filter builtins.isList
+    |> builtins.map (m: {
       "${lib.toLower (builtins.elemAt m 0)}" = {
         "${lib.toLower (builtins.elemAt m 1)}" = builtins.elemAt m 2;
       };
-    }))
-    (builtins.foldl' lib.recursiveUpdate { })
-    (lib.mapAttrs' (
+    })
+    |> builtins.foldl' lib.recursiveUpdate { }
+    |> lib.mapAttrs' (
       name: value: {
         name = "${name}/${builtins.baseNameOf value.url}";
         value = builtins.fetchurl value;
       }
-    ))
-    (pkgs.linkFarm "deps")
-  ];
+    )
+    |> pkgs.linkFarm "deps";
   buildpath = builtins.path {
     path = neovim-src;
     name = "neovim-deps-build-path";
