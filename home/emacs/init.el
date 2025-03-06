348b1d539dfc3d6a2b04207fbe7d9752c79e0175
kuuote <znmxodq1@gmail.com>
2025-03-06 15:40:58 +0900

    Emacs: 起動時にdash.el使うのやめる

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -1,3 +1,7 @@
+(defmacro pipe (name &rest exprs)
+  (declare (indent 1))
+  "anaphoric threading macro"
+  `(let* ,(mapcar (lambda (e) `(,name ,e)) exprs) ,name))
 (cond
  ((null (getenv "babel"))
   (add-to-list 'load-path "@site_lisp@")
@@ -5,14 +9,13 @@
   (require 'init))
  (t
   (find-file-read-only (file-truename "~/.emacs.d/init.org"))
-  (require 'dash)
-  (-->
-   (org-babel-tangle-collect-blocks)
-   (mapcan 'cdr it) ;; per files => per blocks
-   (seq-filter (lambda (a) (string= (car a) "emacs-lisp")) it)
-   (mapcar (-cut elt <> 6) it)
-   (string-join `("(progn" ,@it ")") "\n")
-   (car (read-from-string it))
-   (progn
-     (kill-buffer)
-     (eval it)))))
+  (pipe it
+    (org-babel-tangle-collect-blocks)
+    (mapcan 'cdr it) ;; per files => per blocks
+    (seq-filter (lambda (a) (string= (car a) "emacs-lisp")) it)
+    (mapcar (lambda (a) (elt a 6)) it)
+    (string-join `("(progn" ,@it ")") "\n")
+    (car (read-from-string it))
+    (progn
+      (kill-buffer)
+      (eval it)))))
