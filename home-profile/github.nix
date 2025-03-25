6158499cf08c00ba530d61f61981ecf01af25d23
kuuote <znmxodq1@gmail.com>
2025-03-25 21:53:32 +0900

    ビルド減らした

--- home-profile/github.nix
+++ home-profile/github.nix
@@ -4,18 +4,17 @@
     ../home/base.nix
     ../home/linux.nix
     # keep-sorted start
+    ../home/cmd
+    ../home/emacs-min
     ../home/fish
     ../home/git
-    ../home/inputs.nix
     ../home/nix.nix
     ../home/vars.nix
-    ../home/vim
     # keep-sorted end
     (
       { pkgs, ... }:
       {
         home.packages = [ pkgs.nix-output-monitor ];
-        home.sessionVariables.LANG = "en_US.UTF-8";
       }
     )
   ];
