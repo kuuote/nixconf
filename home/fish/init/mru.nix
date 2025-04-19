2b432f528b124abd0f0f695206a12167a986ab1c
kuuote <znmxodq1@gmail.com>
2025-04-19 20:43:33 +0900

    :fish: cdした時にdirprevも記録しておく

--- home/fish/init/mru.nix
+++ home/fish/init/mru.nix
@@ -3,6 +3,13 @@
 }:
 # https://www.reddit.com/r/fishshell/comments/t8xssl/comment/hzu7fil/
 ''
+  function record_dirprev --on-variable dirprev
+    if test "$dirprev[1]" != ""
+      ${pkgs.python3}/bin/python ${./.}/mru.py ~/.stack $dirprev[1]
+      ${pkgs.python3}/bin/python ${./.}/mru.py ~/.stack $PWD
+    end
+  end
+
   function record_pwd --on-variable PWD
     ${pkgs.python3}/bin/python ${./.}/mru.py ~/.stack $PWD
   end
