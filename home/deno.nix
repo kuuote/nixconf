88fc351b9a10b336189a9478a42d528f34511ff0
kuuote <znmxodq1@gmail.com>
2025-06-20 19:09:51 +0900

    nixpkgsのDenoぶっ壊れてるのでとりあえず上書き

--- home/deno.nix
+++ home/deno.nix
@@ -2,7 +2,16 @@
   pkgs,
   ...
 }:
+let
+  nixpkgs = pkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "3e3afe5174c561dee0df6f2c2b2236990146329f";
+    hash = "sha256-frdhQvPbmDYaScPFiCnfdh3B/Vh81Uuoo0w5TkWmmjU=";
+  };
+  pkgs' = import nixpkgs { system = pkgs.system; };
+in
 {
-  home.packages = [ pkgs.deno ];
+  home.packages = [ pkgs'.deno ];
   home.sessionVariables.DENO_NO_UPDATE_CHECK = "true";
 }
