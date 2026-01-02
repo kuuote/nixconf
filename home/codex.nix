9051eb4d266ff77306f7053580a48b6e5c203237
kuuote <znmxodq1@gmail.com>
2026-01-02 21:37:56 +0900

    Codex: なんか書き方変わっとる

--- home/codex.nix
+++ home/codex.nix
@@ -7,10 +7,10 @@
   kuuote.programs.codex = {
     enable = true;
     config = {
-      network_access = true;
-      tools = {
-        web_search = true;
+      features = {
+        web_search_request = true;
       };
+      network_access = true;
     };
   };
 }
