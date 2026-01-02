ddaabbcc897d92512fdf2ea0e6cacbfde2c7652e
kuuote <znmxodq1@gmail.com>
2026-01-02 22:03:10 +0900

    keyd完全に理解した

--- nixos/keyd.nix
+++ nixos/keyd.nix
@@ -6,7 +6,14 @@
         ids = [ "*" ];
         settings = {
           main = {
-            capslock = "overload(control, esc)";
+            # keep-sorted start
+            capslock = "overload(control, esc)"; # Aの横にあるキーはCtrlなの！！
+            enter = "overload(alt, enter)";
+            tab = "overload(meta, tab)";
+            # keep-sorted end
+
+            muhenkan = "left";
+            henkan = "overload(control, right)";
           };
         };
       };
